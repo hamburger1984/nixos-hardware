4b74f590566019b3f8ad855db108076cf4e3e66d
Andreas K. <hamburger1984@gmail.com>
2025-03-26 12:20:39 +0100

    zenpower seems to abandoned - let's not try to used it.

--- minisforum/um690/default.nix
+++ minisforum/um690/default.nix
@@ -4,7 +4,6 @@
     ../../common/gpu/amd/default.nix
     ../../common/cpu/amd/default.nix
     ../../common/cpu/amd/pstate.nix
-    ../../common/cpu/amd/zenpower.nix
     ../../common/pc/ssd/default.nix
   ];
 }
