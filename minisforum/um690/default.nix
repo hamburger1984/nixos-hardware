33243bf4ba17bc0c27ef9d6e6d9c9f1dbd6bbc1c
Andreas K. <hamburger1984@gmail.com>
2025-03-26 12:07:18 +0100

    adding basic module for minis-forum um690

---
+++ minisforum/um690/default.nix
@@ -0,0 +1,10 @@
+{ ... }:
+{
+  imports = [
+    ../../common/gpu/amd/default.nix
+    ../../common/cpu/amd/default.nix
+    ../../common/cpu/amd/pstate.nix
+    ../../common/cpu/amd/zenpower.nix
+    ../../common/pc/ssd/default.nix
+  ];
+}
