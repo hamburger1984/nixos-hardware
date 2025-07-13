8197a9dc934d0bd1ab25863a581e40a285affe34
Andreas K. <hamburger1984@gmail.com>
2025-07-13 19:59:22 +0200

    Disable zenpower - might be broken

--- minisforum/um690/default.nix
+++ minisforum/um690/default.nix
@@ -4,7 +4,7 @@
     ../../common/gpu/amd/default.nix
     ../../common/cpu/amd/default.nix
     ../../common/cpu/amd/pstate.nix
-    ../../common/cpu/amd/zenpower.nix
+    #../../common/cpu/amd/zenpower.nix
     ../../common/pc/ssd/default.nix
   ];
 }
